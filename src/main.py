9550db162886b24ba0cfd87644f41fb42d8e9806
dianhenglau <dianhenglau@gmail.com>
2018-12-28 16:38:44 +0800

    FIX: Added ASCII art for entering and exiting system

--- src/main.py
+++ src/main.py
@@ -1,10 +1,24 @@
 from step_functions import main_menu
 from helpers import print_breadcrumb
 
-print('''\
-========================
- Ferry Ticketing System
-========================\
+print('''
+    dMMMMMP dMMMMMP dMMMMb  dMMMMb  dMP dMP                        
+   dMP     dMP     dMP.dMP dMP.dMP dMP.dMP                         
+  dMMMP   dMMMP   dMMMMK" dMMMMK"  VMMMMP                          
+ dMP     dMP     dMP"AMF dMP"AMF dA .dMP                           
+dMP     dMMMMMP dMP dMP dMP dMP  VMMMP"                            
+                                                                   
+ dMMMMMMP dMP .aMMMb  dMP dMP dMMMMMP dMMMMMMP dMP dMMMMb  .aMMMMP 
+   dMP   amr dMP"VMP dMP.dM" dMP        dMP   amr dMP dMP dMP"     
+  dMP   dMP dMP     dMMMK"  dMMMP      dMP   dMP dMP dMP dMP MMP"  
+ dMP   dMP dMP.aMP dMP"AM" dMP        dMP   dMP dMP dMP dMP.dMP    
+dMP   dMP  VMMMP" dMP dMP dMMMMMP    dMP   dMP dMP dMP  VMMMP"     
+                                                                   
+   .dMMMb  dMP dMP .dMMMb dMMMMMMP dMMMMMP dMMMMMMMMb              
+  dMP" VP dMP.dMP dMP" VP   dMP   dMP     dMP"dMP"dMP              
+  VMMMb   VMMMMP  VMMMb    dMP   dMMMP   dMP dMP dMP               
+dP .dMP dA .dMP dP .dMP   dMP   dMP     dMP dMP dMP                
+VMMMP"  VMMMP"  VMMMP"   dMP   dMMMMMP dMP dMP dMP                 
 ''')
 
 context = {}
@@ -30,4 +44,10 @@ while i >= 0:
             if callable(steps[i]):
                 break
 
-print('\nSystem quit')
+print('''
+ dMMMMMMP dMP dMP .aMMMb  dMMMMb  dMP dMP     dMP dMP .aMMMb  dMP dMP 
+   dMP   dMP dMP dMP"dMP dMP dMP dMP.dM"     dMP.dMP dMP"dMP dMP dMP  
+  dMP   dMMMMMP dMMMMMP dMP dMP dMMMK"       VMMMMP dMP dMP dMP dMP   
+ dMP   dMP dMP dMP dMP dMP dMP dMP"AM"     dA .dMP dMP.aMP dMP.aMP    
+dMP   dMP dMP dMP dMP dMP dMP dMP dMP      VMMMP"  VMMMP"  VMMMP"     
+''')
